6cde0a28c6aa7e513dcc87d26de551fa31e3be72
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-01-30 00:56:46 -0500

    Added animation to arrow of DropDownMenu shared component

--- src/components/SharedComponents/DropDownMenu.tsx
+++ src/components/SharedComponents/DropDownMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from "react";
 import { ArrowIcon } from "../../assets/icons";
+import { motion } from "motion/react";
 
 interface DropDownMenuProps {
   isOpen: boolean;
@@ -38,14 +39,21 @@ const DropDownMenu: React.FC<DropDownMenuProps> = ({
     [setSelectedId]
   );
   return (
-    <button
+    <motion.button
       className={`flex text-left flex-start w-full relative text-black bg-transparent border-none px-0 focus:outline-none ${buttonStyle}`}
       onClick={handleClick}
     >
       <span className="flex gap-x-1">
         {selectedItem?.icon}
         {selectedItem?.label}
-        <ArrowIcon className="w-4 h-4 self-center" />
+        <motion.span
+          className="w-4 h-4 self-center"
+          initial={{ rotate: 0 }}
+          animate={{ rotate: isOpen ? 180 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <ArrowIcon className="w-4 h-4 self-center" />
+        </motion.span>
       </span>
       {isOpen && (
         <div className={`absolute bottom-3/4 border border-2 bg-white`}>
@@ -64,7 +72,7 @@ const DropDownMenu: React.FC<DropDownMenuProps> = ({
           </ul>
         </div>
       )}
-    </button>
+    </motion.button>
   );
 };
 
