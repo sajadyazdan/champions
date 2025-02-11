bc673261660830a5b751fa020fc70faaa54df029
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-10 22:56:01 -0500

    Added text above the slideshow buttons

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -10,13 +10,18 @@ const MainPage: React.FC = () => {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="absolute z-20 w-full h-full flex items-center justify-center space-x-4">
-        <button className="w-36 h-10 bg-black text-white border border-transparent hover:bg-white hover:text-black hover:border-black">
-          MENS
-        </button>
-        <button className="w-36 h-10 bg-white text-black border border-transparent hover:bg-black hover:text-white">
-          WOMENS
-        </button>
+      <div className="absolute z-20 w-full h-full flex flex-col items-center justify-center space-x-4 flex-row">
+        <div className="m-10">
+          <h2 className="text-[40px]">New Blanks Just Dropped</h2>
+        </div>
+        <div className="flex space-x-4">
+          <button className="w-36 h-10 bg-black text-white border border-transparent hover:bg-white hover:text-black hover:border-black">
+            MENS
+          </button>
+          <button className="w-36 h-10 bg-white text-black border border-transparent hover:bg-black hover:text-white">
+            WOMENS
+          </button>
+        </div>
       </div>
     </div>
   );
