8aed1bca5ca9c09c19571bcd42cd9306e1106b1c
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-21 17:19:17 -0500

    Add Social Media bar to the Second Header

--- src/components/SecondHeader/SecondHeader.tsx
+++ src/components/SecondHeader/SecondHeader.tsx
@@ -1,10 +1,12 @@
+import SocialMedia from "../SocialMedia/SocialMedia";
+
 const SecondHeader: React.FC = () => {
   return (
     <header
       className="flex items-center justify-between px-6 py-4 border-gray-200 flex-shrink-0 relative"
       style={{ height: "40px", backgroundColor: "rgba(16,16,16,0.03)" }}
     >
-      This is the second header
+      <SocialMedia />
     </header>
   );
 };
