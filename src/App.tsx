29613d3af2562fffbcaa8af1efd6856b113a1d5e
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-20 23:47:19 -0500

    Add second header

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import MainPage from "./components/MainPage";
+import SecondHeader from "./components/SecondHeader/SecondHeader";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <SecondHeader />
       <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MainPage />
     </>
