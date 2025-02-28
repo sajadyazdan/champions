138d82782abcb1b4849a8c01ed892c60d0cab3bd
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-02-27 23:48:57 -0500

    Make second header text dynamic

--- src/components/SecondHeader/SecondHeader.tsx
+++ src/components/SecondHeader/SecondHeader.tsx
@@ -1,9 +1,24 @@
+import { useCallback, useState } from "react";
 import { ArrowIcon } from "../../assets/icons";
 import CurrencyDropwdownMenu from "../CurrencyDropdownMenu/CurrentcyDropdownMenu";
 import LanguageDropdownMenu from "../LanguageDropdownMenu/LanguageDropdownMenu";
 import SocialMedia from "../SocialMedia/SocialMedia";
 
+const dynamicTexts = [
+  "WELCOME TO OUR STORE",
+  "FREE SHIPPING ON ORDERS OVER £75",
+];
+
 const SecondHeader: React.FC = () => {
+  const [textIndex, setTextIndex] = useState(0);
+  const handleRightArrow = useCallback(() => {
+    setTextIndex((prev: number) => (prev + 1) % dynamicTexts.length);
+  }, []);
+  const handleLeftArrow = useCallback(() => {
+    setTextIndex((prev: number) =>
+      prev === 0 ? dynamicTexts.length - 1 : prev - 1
+    );
+  }, []);
   return (
     <header
       className="flex items-center justify-between px-6 py-4 border-gray-200 flex-shrink-0 relative"
@@ -13,9 +28,12 @@ const SecondHeader: React.FC = () => {
         <SocialMedia />
       </div>
       <div className="flex text-xs">
-        <ArrowIcon className="w-4 h-4 rotate-90" />{" "}
-        <div className="px-36">WELCOME TO OUR STORE</div>
-        <ArrowIcon className="w-4 h-4 -rotate-90" />{" "}
+        <ArrowIcon className="w-4 h-4 rotate-90" onClick={handleLeftArrow} />{" "}
+        <div className="px-36 w-lg">{dynamicTexts[textIndex]}</div>
+        <ArrowIcon
+          className="w-4 h-4 -rotate-90"
+          onClick={handleRightArrow}
+        />{" "}
       </div>
       <div className="flex space-x-4">
         <CurrencyDropwdownMenu downwardDirection />
