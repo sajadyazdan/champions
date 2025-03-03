2b36c097106bc6dfec96b36d48ef4e38c0bd8f15
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-03-02 23:42:24 -0500

    Add animation to the text of second header

--- src/components/SecondHeader/SecondHeader.tsx
+++ src/components/SecondHeader/SecondHeader.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { ArrowIcon } from "../../assets/icons";
 import CurrencyDropwdownMenu from "../CurrencyDropdownMenu/CurrentcyDropdownMenu";
 import LanguageDropdownMenu from "../LanguageDropdownMenu/LanguageDropdownMenu";
@@ -8,6 +9,12 @@ const dynamicTexts = [
   "WELCOME TO OUR STORE",
   "FREE SHIPPING ON ORDERS OVER £75",
 ];
+
+const textVariants = {
+  initial: { opacity: 0, x: -50 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: 50 },
+};
 
 const SecondHeader: React.FC = () => {
   const [textIndex, setTextIndex] = useState(0);
@@ -31,14 +38,26 @@ const SecondHeader: React.FC = () => {
         <ArrowIcon
           className="w-4 h-4 rotate-90 cursor-pointer"
           onClick={handleLeftArrow}
-        />{" "}
-        <div className="w-2/3">
-          <div>{dynamicTexts[textIndex]}</div>
+        />
+        <div className="w-2/3 overflow-hidden relative">
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={textIndex}
+              variants={textVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{ duration: 0.5 }}
+              className="absolute w-full text-center"
+            >
+              {dynamicTexts[textIndex]}
+            </motion.div>
+          </AnimatePresence>
         </div>
         <ArrowIcon
           className="w-4 h-4 -rotate-90 cursor-pointer"
           onClick={handleRightArrow}
-        />{" "}
+        />
       </div>
       <div className="flex space-x-4">
         <CurrencyDropwdownMenu downwardDirection />
