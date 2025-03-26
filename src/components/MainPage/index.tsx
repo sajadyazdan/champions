5236aa35640d83ac66f49109dfe6c406e35c36d8
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-03-25 20:23:26 -0400

    Correct position of buttons in the main page slider

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -10,25 +10,25 @@ const MainPage: React.FC = () => {
   ];
   return (
     <>
-      <div className="relative w-full h-full">
-        <div className="absolute z-10 w-full h-full">
+      <div className="relative w-full">
+        <div className="relative z-10 w-full h-full">
           <img
             src="./images/main-image.webp"
             alt="Description of image"
-            className="w-full aspect-auto object-cover"
+            className="w-full h-full -z-10 aspect-auto object-cover"
           />
-        </div>
-        <div className="absolute z-20 w-full h-full flex flex-col items-center justify-center space-x-4 flex-row">
-          <div className="m-10">
-            <h2 className="text-[40px]">New Blanks Just Dropped</h2>
-          </div>
-          <div className="flex space-x-4">
-            <button className="w-36 h-10 bg-black text-white border border-transparent hover:bg-white hover:text-black hover:border-black">
-              MENS
-            </button>
-            <button className="w-36 h-10 bg-white text-black border border-transparent hover:bg-black hover:text-white">
-              WOMENS
-            </button>
+          <div className="absolute z-20 w-full h-full flex flex-col items-center justify-center top-0 space-x-4 flex-row">
+            <div className="m-10">
+              <h2 className="text-[40px]">New Blanks Just Dropped</h2>
+            </div>
+            <div className="flex space-x-4">
+              <button className="w-36 h-10 bg-black text-white border border-transparent hover:bg-white hover:text-black hover:border-black">
+                MENS
+              </button>
+              <button className="w-36 h-10 bg-white text-black border border-transparent hover:bg-black hover:text-white">
+                WOMENS
+              </button>
+            </div>
           </div>
         </div>
       </div>
