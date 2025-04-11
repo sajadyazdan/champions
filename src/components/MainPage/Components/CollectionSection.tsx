84a6d82655692a526c64e5e3e9696f486892dcaa
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-04-10 23:33:32 -0400

    Start Collection Section

---
+++ src/components/MainPage/Components/CollectionSection.tsx
@@ -0,0 +1,5 @@
+function CollectionSection() {
+  return <h2 className="text-3xl mb-8">You may love our "COLLECTIONS"</h2>;
+}
+
+export default CollectionSection;
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NewsTicker from "../NewsTicker/NewsTicker";
 import DiscoverSection from "./Components/DiscoverSection";
+import CollectionSection from "./Components/CollectionSection";
 
 const MainPage: React.FC = () => {
   return (
@@ -39,6 +40,7 @@ const MainPage: React.FC = () => {
         </div>
       </section>
       <DiscoverSection />
+      <CollectionSection />
       <section className="relative w-full flex justify-center items-center py-16 bg-[#efefef]">
         <div className="flex flex-col justify-center items-center text-center space-y-4 max-w-3xl">
           <p className="break-words">
