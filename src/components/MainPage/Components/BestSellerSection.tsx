a0211dfe42d07e692bf7f5687db10aa6ce89491a
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-04-23 23:56:45 -0400

    Add base images to BestSeller Section

--- src/components/MainPage/Components/BestSellerSection.tsx
+++ src/components/MainPage/Components/BestSellerSection.tsx
@@ -14,7 +14,7 @@ const ItemsList = [
     discount: 10,
     title: "OVERSIZED TEE - BLACK",
     availableColors: [
-      { color: Color.BLACK, src: "./images/product-1-1.webp" },
+      { color: Color.BLACK, src: "./images/product-17.webp" },
       { color: Color.BEIGE, src: "./images/product-1-beige.webp" },
       { color: Color.GREY, src: "./images/product-1-grey.webp" },
       { color: Color.PEARL_PINK, src: "./images/product-1-pearlpink.webp" },
@@ -28,7 +28,7 @@ const ItemsList = [
     price: 30.0,
     discount: 0,
     title: "PURE COTTON HEAVYWEIGHT T-SHIRT",
-    availableColors: [{ color: Color.BEIGE, src: "./images/product-2-1.webp" }],
+    availableColors: [{ color: Color.BEIGE, src: "./images/product-18.webp" }],
     onSale: true,
   },
   {
@@ -39,7 +39,7 @@ const ItemsList = [
     discount: 5,
     title: "OVERSIZED TEE - PEARL PINK",
     availableColors: [
-      { color: Color.PEARL_PINK, src: "./images/product-3-1.webp" },
+      { color: Color.PEARL_PINK, src: "./images/product-19.webp" },
       { color: Color.BROWN, src: "./images/product-3-brown.webp" },
     ],
     onSale: false,
@@ -52,7 +52,7 @@ const ItemsList = [
     discount: 20,
     title: "LOOSE FIT CREW-NECK COTTON T-SHIRT",
     availableColors: [
-      { color: Color.GREY, src: "./images/product-4-1.webp" },
+      { color: Color.GREY, src: "./images/product-20.webp" },
       { color: Color.PINK, src: "./images/product-4-pink.webp" },
     ],
     onSale: false,
