231ab9d43c834f4e3fb61bd2dab3e43ebade78e4
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-04-30 23:46:26 -0400

    Complete the structure of SweatsAndDenimSection

--- src/components/MainPage/Components/SweatsAndDenimSection.tsx
+++ src/components/MainPage/Components/SweatsAndDenimSection.tsx
@@ -4,8 +4,20 @@ const SweatsAndDenimSection = () => {
       <div>
         <img src="./images/multirow-img-1.webp" />
       </div>
-      <div>
-        <h2>Discover Sweats</h2>
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="mb-4">Discover Sweats</h2>
+        <p className="w-2/3 mb-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestiae, cumque, perferendis praesentium nisi soluta eius dolorum
+          odio quidem exercitationem blanditiis quibusdam quo modi vitae error
+          est, iure inventore voluptate.
+        </p>
+        <button
+          className="w-48 h-16 self-center text-white text-lg mb-12 rounded-none"
+          style={{ backgroundColor: "rgb(16,16,16)" }}
+        >
+          SHOP NOW
+        </button>
       </div>
     </div>
   );
