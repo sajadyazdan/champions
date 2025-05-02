7db4da640f7d3656153e3001de00bfcd0f872644
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-05-01 23:55:15 -0400

    Add more items to SweatsAndDenimSection

--- src/components/MainPage/Components/SweatsAndDenimSection.tsx
+++ src/components/MainPage/Components/SweatsAndDenimSection.tsx
@@ -1,24 +1,40 @@
+const items = [
+  {
+    id: 1,
+    title: "Discover Sweats",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestiae, cumque, perferendis praesentium nisi soluta eius dolorum odio quidem exercitationem blanditiis quibusdam quo modi vitae error est, iure inventore voluptate.",
+    src: "./images/multirow-img-1.webp",
+  },
+  {
+    id: 2,
+    title: "Discover Denim",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestiae, cumque, perferendis praesentium nisi soluta eius dolorum odio quidem exercitationem blanditiis quibusdam quo modi vitae error est, iure inventore voluptate.",
+    src: "./images/multirow-img-2.webp",
+  },
+];
+
 const SweatsAndDenimSection = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 p-10">
-      <div>
-        <img src="./images/multirow-img-1.webp" />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h2 className="mb-4">Discover Sweats</h2>
-        <p className="w-2/3 mb-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          molestiae, cumque, perferendis praesentium nisi soluta eius dolorum
-          odio quidem exercitationem blanditiis quibusdam quo modi vitae error
-          est, iure inventore voluptate.
-        </p>
-        <button
-          className="w-48 h-16 self-center text-white text-lg mb-12 rounded-none"
-          style={{ backgroundColor: "rgb(16,16,16)" }}
-        >
-          SHOP NOW
-        </button>
-      </div>
+    <div>
+      {items.map((item) => (
+        <div key={item.id} className="grid grid-cols-2 gap-4 p-10">
+          <div>
+            <img src={item.src} alt={item.title} />
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="mb-4">{item.title}</h2>
+            <p className="w-2/3 mb-8">{item.description}</p>
+            <button
+              className="w-48 h-16 self-center text-white text-lg mb-12 rounded-none"
+              style={{ backgroundColor: "rgb(16,16,16)" }}
+            >
+              SHOP NOW
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
