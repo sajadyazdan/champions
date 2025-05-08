a312867898865df82d99bf2d3173b0376e9486d9
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-05-07 23:30:23 -0400

    Correct the content of elements in LatestNewsSection

--- src/components/MainPage/Components/LatestNewsSection.tsx
+++ src/components/MainPage/Components/LatestNewsSection.tsx
@@ -7,14 +7,14 @@ const items = [
   },
   {
     id: 2,
-    src: "images/blog-6.webp",
-    title: "THE BEST MEN’S LINEN SUIT FOR SUMMER",
+    src: "images/blog-5.webp",
+    title: "THE BEST BUDGET MEN’S CLOTHING BRANDS",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt quas facere debitis, placeat ea eaque. Est cupiditate, assumenda odio vitae, dolorum necessitatibus sit praesentium excepturi, voluptate omnis beatae cum natus fugiat deleniti voluptates soluta tempore! Ex quasi autem illum quisquam! Asperiores animi nisi perferendis quod nobis explicabo laborum nulla accusantium!",
   },
   {
     id: 3,
-    src: "images/blog-6.webp",
-    title: "THE BEST MEN’S LINEN SUIT FOR SUMMER",
+    src: "images/blog-4.webp",
+    title: "23 THINGS NO MODERN MAN SHOULD OWN IN 2023",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt quas facere debitis, placeat ea eaque. Est cupiditate, assumenda odio vitae, dolorum necessitatibus sit praesentium excepturi, voluptate omnis beatae cum natus fugiat deleniti voluptates soluta tempore! Ex quasi autem illum quisquam! Asperiores animi nisi perferendis quod nobis explicabo laborum nulla accusantium!",
   },
 ];
@@ -24,13 +24,13 @@ const LatestNewsSection = () => {
     <section className="relative w-full flex pb-8 px-8 bg-white">
       <div className="flex flex-col text-left">
         <h2 className="text-3xl mb-8">Latest news</h2>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-x-1">
           {items.map((item) => (
             <div key={item.id} className="flex flex-col">
-              <img src={item.src} alt={item.title} className="mb-4" />
+              <img src={item.src} alt={item.title} className="mb-6" />
               <div className="flex flex-col">
                 <h3 className="mb-4">{item.title}</h3>
-                <p>{item.text}</p>
+                <p class="line-clamp-4">{item.text}</p>
               </div>
             </div>
           ))}
