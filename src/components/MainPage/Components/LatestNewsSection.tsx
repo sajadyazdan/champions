7455e86372d7fa2dcf3f261657a684494146657e
Yazdanparast <sajadyazdanparast1@gmail.com>
2025-05-08 23:08:47 -0400

    Add animation to LatesNewsSection

--- src/components/MainPage/Components/LatestNewsSection.tsx
+++ src/components/MainPage/Components/LatestNewsSection.tsx
@@ -1,3 +1,5 @@
+import { motion } from "motion/react";
+
 const items = [
   {
     id: 1,
@@ -19,24 +21,55 @@ const items = [
   },
 ];
 
+const parentVariants = {
+  // Removed hover state from parent
+};
+
+const childVariants = {
+  hover: { scale: 1.025 },
+  initial: { scale: 1 },
+};
+
+const titleVariants = {
+  hover: { textDecoration: "underline" },
+  initial: { textDecoration: "none" },
+};
+
 const LatestNewsSection = () => {
   return (
-    <section className="relative w-full flex pb-8 px-8 bg-white">
+    <motion.section
+      className="relative w-full flex pb-8 px-8 bg-white"
+      variants={parentVariants}
+      initial="initial"
+    >
       <div className="flex flex-col text-left">
         <h2 className="text-3xl mb-8">Latest news</h2>
         <div className="grid grid-cols-3 gap-x-1">
           {items.map((item) => (
-            <div key={item.id} className="flex flex-col">
-              <img src={item.src} alt={item.title} className="mb-6" />
-              <div className="flex flex-col">
-                <h3 className="mb-4">{item.title}</h3>
-                <p class="line-clamp-4">{item.text}</p>
+            <motion.div
+              key={item.id}
+              className="flex flex-col"
+              whileHover="hover"
+            >
+              <div className="mb-6 overflow-hidden">
+                <motion.img
+                  src={item.src}
+                  alt={item.title}
+                  variants={childVariants}
+                />
               </div>
-            </div>
+
+              <div className="flex flex-col">
+                <motion.h3 className="mb-4" variants={titleVariants}>
+                  {item.title}
+                </motion.h3>
+                <p className="line-clamp-4">{item.text}</p>
+              </div>
+            </motion.div>
           ))}
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
